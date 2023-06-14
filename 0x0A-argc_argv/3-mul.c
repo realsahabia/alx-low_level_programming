d4368115cbddb4dbc27f0474fc5c9a1d8d481ebf
realsahabia <yakubukarim12@gmail.com>
2023-06-14 07:55:56 -0700

    program that multiplies two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _atoi - progrsm that converts a string to an integer.
+ * @s: string to be converted.
+ * Return: the integer converted from the string.
+ */
+
+int _atoi(char *s)
+{
+	int counter, d, sum, len, f, digit;
+
+	counter = 0;
+	d = 0;
+	sum = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (counter < len && f == 0)
+	{
+		if (s[counter] == '-')
+			++d;
+
+		if (s[counter] >= '0' && s[counter] <= '9')
+		{
+			digit = s[counter] - '0';
+
+			if (d % 2)
+				digit = -digit;
+
+			sum = sum * 10 + digit;
+
+			f = 1;
+
+			if (s[counter + 1] < '0' || s[counter + 1] > '9')
+				break;
+
+			f = 0;
+		}
+		counter++;
+	}
+
+	if (f == 0)
+		return (0);
+
+	return (sum);
+}
