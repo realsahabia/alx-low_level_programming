8b28f9aad85b428249bb1a52dfe54f2ca5d8017e
realsahabia <yakubukarim12@gmail.com>
2023-06-19 13:33:20 -0700

    header file that contains struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - new struct for dog info
+ * @name: first element of struct
+ * @age: second elememnt of struct
+ * @owner: third element of sstruct.
+ * Description: describe info for dog
+ */
+
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+};
+
+#endif
