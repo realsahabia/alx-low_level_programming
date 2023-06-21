61fa1d4b4824f03847e777ef3ac48e3ffcb72d82
realsahabia <yakubukarim12@gmail.com>
2023-06-21 07:17:21 -0700

    function with a parameter of function pointer that prints a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,6 @@
 #include "function_pointers.h"
 /**
- * print_name - function that takes a char* and function* 
+ * print_name - function that takes a char* and function*
  * and prints a name
  * @name: name to parameter to be printed
  * @f: a function pointer to a function that prints a string
