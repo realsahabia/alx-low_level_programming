3274f9e18a1957243f7c6137ace25c50f4219a03
realsahabia <yakubukarim12@gmail.com>
2023-07-01 06:56:25 -0700

    updated

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,7 +15,7 @@ size_t print_list(const list_t *h)
 		if (!h->str)
 			printf("[0] (nil)\n");
 		else
-			printf("%s[%u]\n", h->str, h->len);
+			printf("[%u]%s\n", h->len, h->str);
 
 		h = h->next;
 		i++;
