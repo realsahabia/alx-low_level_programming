5267f78b7417a9ea512949fad29481a9f5b86729
Sahabia Yakubu <34670439+realsahabia@users.noreply.github.com>
2023-07-12 02:21:05 +0000

    Create print_magic.c

---
+++ 0x15-file_io/print_magic.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_magic - Prints the magic numbers of an ELF header.
+ * @e_ident: A pointer to an array containing the ELF magic numbers.
+ *
+ * Description: Magic numbers are separated by spaces.
+ */
+void print_magic(unsigned char *e_ident)
+{
+	int i;
+
+	printf(" Magic: ");
+
+	for (i = 0; i < EI_NIDENT; i++)
+	{
+		printf("%02x", e_ident[i]);
+
+		if (i == EI_NIDENT - 1)
+			printf("\n");
+		else
+			printf(" ");
+	}
+}
