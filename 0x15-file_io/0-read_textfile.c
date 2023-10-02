851ffe3797d6d7d74880d2e358785f11f74d3891
realsahabia <yakubukarim12@gmail.com>
2023-10-02 16:09:44 -0700

    initials

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,40 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+ssize_t read_textfile(const char *filename, size_t letters) {
+    if (filename == NULL)
+        return 0;
+
+    int fd = open(filename, O_RDONLY);
+
+    if (fd == -1)
+        return 0;
+
+    char *buffer = malloc(letters);
+
+    if (buffer == NULL) {
+        close(fd);
+        return 0;
+    }
+
+    ssize_t bytes_read = read(fd, buffer, letters);
+
+    if (bytes_read == -1) {
+        free(buffer);
+        close(fd);
+        return 0;
+    }
+
+    ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+
+    free(buffer);
+    close(fd);
+
+    if (bytes_written == -1 || bytes_written != bytes_read)
+        return 0;
+
+    return bytes_written;
+}
